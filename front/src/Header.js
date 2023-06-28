34cb2bd8bb3eb20a9616a9e14696205d63328f2d
Maksym Dibrov <maksym.dibrov@solidgate.com>
2023-06-28 18:50:52 +0200

    initial commit

---
+++ front/src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Header.css'
+
+function Header() {
+    return (
+        <div className="top_menu">
+            <div className="title">May fortune's embrace guide your steps, brave wanderer, as you venture into realms unknown!</div>
+        </div>
+    )
+}
+
+export {Header}
